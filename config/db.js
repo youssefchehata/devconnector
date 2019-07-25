65fbce8690ac09742b81c3ebbe74a239f938b301
jsf.ch6@gmail.com <jsf.ch6@gmail.com>
2019-07-25 03:48:09 +0100

    clean code

--- config/db.js
+++ config/db.js
@@ -1,17 +1,13 @@
 const mongoose = require('mongoose');
 
+const connectDB = async () => {
+  try {
+    await mongoose
+      .connect('mongodb://localhost/devconnector', { useNewUrlParser: true })
+      .then(() => console.log('connected to MongoDB..'));
+  } catch (err) {
+    console.error('Could not connect to MongoDB...', err);
+  }
+};
 
-const connectDB = async ()=>{
-    try{ 
-        await mongoose
-  .connect('mongodb://localhost/devconnector', { useNewUrlParser: true })
-  .then(() => console.log('connected to MongoDB..'))
-
-    }
-    catch(err){
-     console.error('Could not connect to MongoDB...', err)
-    }
-}
-
- module.exports = connectDB
-
+module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,20 +1,13 @@
 // const Joi = require('joi')
 //  Joi.objectId = require('joi-objectid')(Joi) //objectId
 
-// const mongoose = require('mongoose');
-
 const connectDB = require('./config/db');
 
 const express = require('express');
 const app = express();
 
-// mongoose
-//   .connect('mongodb://localhost/devconnector', { useNewUrlParser: true })
-//   .then(() => console.log('connected to MongoDB..'))
-//   .catch(err => console.error('Could not connect to MongoDB...', err));
-
 app.use(express.json());
-connectDB() // CONNECT TO DATA_BASE
+connectDB(); // CONNECT TO DATA_BASE
 app.get('/', (req, res) => res.send('GET FOR TEST')); //FOR TEST EXPRESS
 
 const port = process.env.PORT || 5000;
