300087c21fa059c22697897a110dcb58f8aa89d7
jsf.ch6@gmail.com <jsf.ch6@gmail.com>
2019-07-25 03:16:24 +0100

    install

---
+++ server.js
@@ -0,0 +1,22 @@
+// const Joi = require('joi')
+//  Joi.objectId = require('joi-objectid')(Joi) //objectId
+
+const mongoose = require('mongoose');
+
+// const genres = require('./routes/genres');
+
+const express = require('express');
+const app = express();
+
+mongoose
+  .connect('mongodb://localhost/devconnector', { useNewUrlParser: true })
+  .then(() => console.log('connected to MongoDB..'))
+  .catch(err => console.error('Could not connect to MongoDB...', err));
+
+app.use(express.json());
+// app.use('/api/genres', genres);
+
+app.get('/', (req, res) => res.send('GET FOR TEST'));
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
