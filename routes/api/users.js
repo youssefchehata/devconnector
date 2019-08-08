dc91c45d514ed122e606ba17cd0b9dd6c9119b42
jsf.ch6@gmail.com <jsf.ch6@gmail.com>
2019-08-08 23:40:48 +0100

    login

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,10 +2,10 @@ const express = require('express');
 const router = express.Router();
 const jwt = require('jsonwebtoken');
 const config = require('config');
-
+const { check, validationResult } = require('express-validator');
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
-const { check, validationResult } = require('express-validator');
+
 
 const User = require('../../models/User');
 
