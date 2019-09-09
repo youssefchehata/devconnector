06923e124912a53530a131677523b50092bed3cc
jsf.ch6@gmail.com <jsf.ch6@gmail.com>
2019-09-10 00:13:30 +0100

    alertComponent&redux

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,12 +1,62 @@
-import React from 'react'
-
-
+import React, { Fragment, useState } from 'react';
+import { Link } from 'react-router-dom';
 const Login = () => {
-    return (
-        <div>
-        <h1>Login</h1>
-        </div>
-    )
-}
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    password2: ''
+  });
+  const { email, password, password2 } = formData;
 
-export default Login
+  const onChange = e =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const onSubmit = async e => {
+    e.preventDefault();
+    if (password !== password2) {
+      console.log('Passwords do not match');
+    } else {
+      console.log('SUCCESS');
+    }
+  };
+  return (
+    <Fragment>
+      <h1 className='large text-primary'>Sign In</h1>
+      <p className='lead'>
+        <i className='fas fa-user'></i> Sign Into Your Account
+      </p>
+      <form className='form' onSubmit={e => onSubmit(e)}>
+        <div className='form-group'></div>
+        <div className='form-group'>
+          <input
+            type='email'
+            placeholder='Email Address'
+            name='email'
+            value={email}
+            onChange={e => onChange(e)}
+            required
+          />
+        </div>
+        <div className='form-group'>
+          <input
+            type='password'
+            placeholder='Password'
+            name='password'
+            value={password}
+            onChange={e => onChange(e)}
+            // @ts-ignore
+            minLength='6'
+          />
+        </div>
+
+        <input type='submit' className='btn btn-primary' value='Login' />
+      </form>
+      <p className='my-1'>
+        Don't have an account? <Link to='/register'>Sign Up</Link>
+      </p>
+    </Fragment>
+  );
+};
+
+export default Login;
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,6 +1,12 @@
 import React, { Fragment, useState } from 'react';
-import axios from 'axios';
-const Register = () => {
+import { connect } from 'react-redux';
+
+import { Link } from 'react-router-dom';
+import { setAlert } from './../../actions/alert';
+
+import PropTypes from 'prop-types';
+
+const Register = ({ setAlert }) => {
   const [formData, setFormData] = useState({
     name: '',
     email: '',
@@ -12,25 +18,12 @@ const Register = () => {
   const onChange = e =>
     setFormData({ ...formData, [e.target.name]: e.target.value });
 
-  const onSubmit =async e => {
+  const onSubmit = async e => {
     e.preventDefault();
     if (password !== password2) {
-      console.log('Passwords do not match');
+      setAlert('Passwords do not match', 'danger');
     } else {
-      console.log(formData);
-      const newUser = {name,email,password}
-      try {
-          const config = {
-              headers:{
-                  'Content-Type':'application/json'
-              }
-          }
-          const body = JSON.stringify(newUser)
-          const res = await axios.post('/api/users',body,config)
-          console.log(res.data);
-      } catch (err) {
-          console.error(err.response.data);
-      }
+      console.log('SUCCESS');
     }
   };
   return (
@@ -89,10 +82,16 @@ const Register = () => {
         <input type='submit' className='btn btn-primary' value='Register' />
       </form>
       <p className='my-1'>
-        Already have an account? <a href='login.html'>Sign In</a>
+        Already have an account? <Link to='/login'>Sign In</Link>
       </p>
     </Fragment>
   );
 };
 
-export default Register;
+Register.propTypes = {
+  setALERT: PropTypes.func.isRequired
+};
+export default connect(
+  null,
+  { setAlert }
+)(Register);
