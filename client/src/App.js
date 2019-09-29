b9d9d894cea228d272ffe00e5132af3f485691ec
jsf.ch6@gmail.com <jsf.ch6@gmail.com>
2019-09-29 18:40:29 +0100

    ProtectedRoute

--- client/src/App.js
+++ client/src/App.js
@@ -13,6 +13,8 @@ import Alert from './components/layout/Alert';
 
 import { loadUser } from './actions/auth';
 import setAuthToken from './utils/setAuthToken';
+import Dashboard from './components/dashboard/Dashboard';
+import PrivateRoute from './components/routing/PrivateRoute';
 
 if (localStorage.token) {
   setAuthToken(localStorage.token);
@@ -34,6 +36,7 @@ const App = () => {
             <Switch>
               <Route exact path='/register' component={Register} />
               <Route exact path='/login' component={Login} />
+              <PrivateRoute exact path='/dashboard' component={Dashboard} />
             </Switch>
           </section>
         </Fragment>
