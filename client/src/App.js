79a73182e9ab041b9b65fcb2325b31b300a805fe
jsf.ch6@gmail.com <jsf.ch6@gmail.com>
2019-11-25 00:47:27 +0100

    create profile

--- client/src/App.js
+++ client/src/App.js
@@ -15,6 +15,8 @@ import { loadUser } from './actions/auth';
 import setAuthToken from './utils/setAuthToken';
 import Dashboard from './components/dashboard/Dashboard';
 import PrivateRoute from './components/routing/PrivateRoute';
+import CreateProfile from './components/profile-forms/CreateProfile';
+
 
 if (localStorage.token) {
   setAuthToken(localStorage.token);
@@ -37,6 +39,7 @@ const App = () => {
               <Route exact path='/register' component={Register} />
               <Route exact path='/login' component={Login} />
               <PrivateRoute exact path='/dashboard' component={Dashboard} />
+              <PrivateRoute exact path='/create-profile' component={CreateProfile} />
             </Switch>
           </section>
         </Fragment>
